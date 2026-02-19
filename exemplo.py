798364e878bd56fc999485330c5d219463327402
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-02-19 08:17:12 -0300

    R3R3
    SARK

--- exemplo.py
+++ exemplo.py
@@ -2,5 +2,15 @@ print("Hello Word")
 import this 
 print(7+6)
 print('7'+'6')
+nome= 'Luvas'
+idade=17
+peso= 1
+print(nome, idade, peso)
+nome=input('qual seu nome?: ')
+idade=input('qual sua idade?: ')
+peso=input('qual seu peso?: ')
+print('Seu nome é {}, sua idade é {} e seu peso é {}'.format(nome, idade, peso))
+
+
 
 
