7fa3b96dd5233ab89d5db3056e429903e66d3e52
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-03 09:50:13 -0300

    ETWE
    WEYH

--- exercicios/atividade01.py
+++ exercicios/atividade01.py
@@ -1,2 +1,4 @@
-print(14+57)
-print('14'+'57')
+num02 = int(input('digite seu segundo numero:'))
+num03 = int(input('digite seu terceiro numero:'))
+soma = num02 + num03
+print('a soma de{} + {}, é {} ' .format ( num02, num03, soma))
--- exercicios/exercicio04.py
+++ exercicios/exercicio04.py
@@ -2,3 +2,7 @@
 float é igual numeros quebrados.
 bool é pra armazenar valores de verdadeiro e falso.
 str armazenar texto."""
+
+num01= int(input('digite seu primeiro numero: '))
+num02= int(input('digite seu segundo numero: '))
+print(num01 + num02)
