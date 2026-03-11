a4d552d9a27d058fc5c1af99968f130d79702007
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-11 10:56:21 -0300

    lkjkjh
    ´]pkõjhv

---
+++ exercicios/exercicio10.py
@@ -0,0 +1,7 @@
+n1 = int(input('quantos metros tem:?'))
+cm = n1*100
+mm= n1*1000
+print('o tamanho foi de {}, os centímetros foi de{}, seus milímetros foi de{}'.format(n1, cm, mm))
+
+
+
---
+++ exercicios/exercicio11.py
@@ -0,0 +1,14 @@
+n1=int(input('os numeros da sua tabuada foi de: ?' ))
+n2= n1*1
+n3=n1*2
+n4=n1*3
+n5=n1*4
+n6=n1*5
+n7=n1*6
+n8=n1*7
+n8=n1*8
+n9=n1*9
+n10=n1*10
+print('os numeros da sua tabuada foi de {} seu numero multiplicado por 1{}seu numero multiplicado por 2{}seu numero multiplicado por 3{}seu numero multiplicado por 4{} seu numero multiplicado por 5{} seu numero multiplicado por 6 {} seu numero multiplicado por 7{} seu numero multiplicado por 8{}seu numero multiplicado por 9{}seu numero multiplicado por 10'.format(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10))
+
+ 
