78fdc4089d90c5281db50a1adaeb70fd24bb77bf
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-16 12:23:41 -0300

    ASAD
    FGFN

---
+++ exercicios/exercicio13.py
@@ -0,0 +1,7 @@
+altura= float(input(' qual sua altura: ?'))
+largura= float(input(' qual é sua largura: ?'))
+area=altura*largura
+tinta=area/2
+print('a sua altura é{}, sua largura é {}, sua area é de {}, e sua tinta final de quantidade de litros é de'.format(altura, largura, area, tinta))
+area=tinta
+
