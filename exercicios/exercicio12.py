74355ac851695fe668eb43809663f62faf0086d2
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-16 08:16:59 -0300

    aafgnbsa
    DAFSGDHNFB

--- exercicios/exercicio12.py
+++ exercicios/exercicio12.py
@@ -1,6 +1,6 @@
-n1= float(input('Digite a quantidade de valor em dolar para ser convertido em real: '))
-
-
+n1= float(input('Digite o valor em dolar para ser convertido em real '))
+n2 = n1*5.38
+print('O valor de dolar digitado foi {}, convertendo do dolar em real, teremos o valor de {}'.format(n1, n2))
 
 
 
