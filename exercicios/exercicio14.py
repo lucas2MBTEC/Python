c4dad2710e698664b664150c54ccd4f789b2a858
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-17 12:27:52 -0300

    5~[hguy
    [´p
    oihugog

---
+++ exercicios/exercicio14.py
@@ -0,0 +1,11 @@
+preco=float(input('qal é o preço do seu produto: ?'))
+desconto= preco - (preco*5/100)
+print('o preço do seu produto era de: {}, com desconto de 5% foi de:{}'.format(preco, desconto))
+
+
+
+
+
+
+
+
---
+++ exercicios/exercicio15.py
@@ -0,0 +1,5 @@
+salario=float(input('seu salario de agora é de: '))
+aumento = salario + (salario*15/100)
+print('seu antigo salario é de: {}, seu salario atual é de:{}'.format(salario,aumento))
+
+
