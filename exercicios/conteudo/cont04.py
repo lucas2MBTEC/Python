41bd46b38d0947726474c9da391bfe84ef2f4e31
lucas2MBTEC <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-24 12:00:24 -0300

    scdv
    asadcfv

--- exercicios/conteudo/cont04.py
+++ exercicios/conteudo/cont04.py
@@ -1,8 +1,12 @@
-#para importar as bibiçhotecas e funcionalidades em python utilizase o comando import seguido do nome da bibilhoteca oumódulo que irá ser carregado esse programa.
+#para importar as bibiçhotecas e funcionalidades em python utilizase o comando import seguido do nome da bibilhoteca oumódulo que irá 3ser carregado esse programa.
 #para importar coisas específicas de uma bibilhoteca utilaza-se o comando from seguiido  o nome da bilhoteca seguido da palavra import seguido do item que desewja importar.
-import math 
-num01= int(input(' digite um numero: ')) 
-raiz= math.sqrt(num01)
-print('a raiz de {} é igual a {}'.format(num01, raiz))
+#import math 
+#num01= int(input(' digite um numero: ')) 
+#raiz= math.sqrt(num01)
+#print('a raiz de {} é igual a {}'.format(num01,math.ceil raiz)) # aredpnda pra cima
+#Print('a raiz de {} é igual a {}'.format(num01,math.floor raiz))# aredomda pra baixo
 
-
+#importando funcionalidades expesificas.
+from math import sqrt, floor,
+num = int(input(''))
+raiz = sqrt(num)
