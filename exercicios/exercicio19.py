dc27e8d148d4883f6761475eaac76ce793871c0f
lucas2MBTEC <lucas.zimovski.brasil@escola.pr.gov.br>
2026-03-30 12:27:12 -0300

    sdav
    dsa

---
+++ exercicios/exercicio19.py
@@ -0,0 +1,7 @@
+from math import hypot
+cat01 = float(input('insira seu cateto de altura:'))
+cat02 = float (input(' insira o seu cateto de largura'))
+hi = (cat01**2+cat02**2)**0.5
+print(' a hipotenusa sera de: {}'.format(hi))
+
+
