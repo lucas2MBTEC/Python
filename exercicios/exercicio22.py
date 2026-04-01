85a9dae8f50abcbb7e73d20729a7d2e771bf3666
lucas2MBTEC <lucas.zimovski.brasil@escola.pr.gov.br>
2026-04-01 09:06:12 -0300

    ea
    r

--- exercicios/exercicio22.py
+++ exercicios/exercicio22.py
@@ -1,10 +1,8 @@
-aluno01 = input('o nome do primeiro aluno foi de: ')
-
-
-
-
-
-
-
-
-
+from random import shuffle
+aluno01 = str(input('primeiro aluno:'))
+aluno02 = str(input('o segundo aluno:'))
+aluno03 = str(input('o terceiro aluno:'))
+aluno04 = str(input('o quarto aluno:'))
+lista = [aluno01, aluno02, aluno03, aluno04]
+shuffle(lista)
+print('a ordem da apresentação será{}'.format(lista))
---
+++ exercicios/exercicio23.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load()
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
