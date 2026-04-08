d8d9416efc25b6e23958df152afd86eaf6d544b2
lucas2MBTEC <lucas.zimovski.brasil@escola.pr.gov.br>
2026-04-08 11:11:21 -0300

    23
    e

---
+++ index01.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Meu primeiro app de casa de apostas 🚀</Text>
+    </View>
+  );
+}
