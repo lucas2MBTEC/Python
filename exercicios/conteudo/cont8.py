0095ca39a6a61ab6ad6888de164cd0a8bf75ce0a
lucas2MBTEC <lucas.zimovski.brasil@escola.pr.gov.br>
2026-04-28 09:48:08 -0300

    fwegngn
    adwegrhn

---
+++ exercicios/conteudo/cont8.py
@@ -0,0 +1,24 @@
+#tempo = int(input("Quantos anos tem seu carro? "))
+
+#if tempo <= 3:
+    #print('carro novo')
+#else:
+   # print('carro velho')
+
+#print('--fim--')
+
+#nome = str(input('Qual é o seu nome? '))
+#if nome == 'Lucas':
+   # print('Que nome lindo você tem!')
+#else:
+    #print('seu nome é tão normal')
+#print('Bom dia {}!'.format(nome))
+
+
+tempo = int(input("Quantos anos tem seu carro? "))
+
+# Espaços adicionados antes e depois do 'else'
+print('carro novo' if tempo <= 3 else 'carro velho')
+
+# 'print' corrigido para letra minúscula
+print('--fim--')
