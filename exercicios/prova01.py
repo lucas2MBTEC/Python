2b4c1aef26a2a8b95dc45c460628af43698503ae
lucas2MBTEC <lucas.zimovski.brasil@escola.pr.gov.br>
2026-04-28 09:20:03 -0300

    eratrhtgnm
    adwfegsfhzdbgnf vnc

--- exercicios/prova01.py
+++ exercicios/prova01.py
@@ -1,7 +1,15 @@
-numero = float(input("Digite um número: "))
-if numero > 0:
-    print("O número é positivo.")
-elif numero < 0:
-    print("O número é negativo.")
-else:
-    print("O número é zero.")
+import pandas as pd
+
+  Alunos = ("Carlos", "Daniela", "Igor", "Gabriel", "Fernanda"),
+    Matemática = (5.1, 8.6, 5.6, 8.0, 10.0,)
+    Português =(9.1, 6.9, 7.8, 5.8, 6.0,)
+    História = (7.3, 9.8, 8.8, 8.5, 7.0,)
+    Geografia =( 6.6, 8.6, 5.5, 8.6, 8.2,)
+    Ciências = (6.5, 7.0, 6.8, 9.2, 7.5)
+
+
+df = pd.read_csv('prova01.csv')
+df
+df.head(Alunos, Matemática, Português, História, Geografia, Ciências)
+df.columns(6)
+df.shape (4)
